e2fe16e605c1b6684affc9d9e1bdad2e22a4059a
Priyanka Sharma <priyanka8930@gmail.com>
2018-06-08 19:44:30 +0200

    :sparkles: Create a static hash map to keep the student data.

--- src/main/java/sharma/priyanka/lawendy/abram/wis/hw2/Assignment2/Assignment2Application.java
+++ src/main/java/sharma/priyanka/lawendy/abram/wis/hw2/Assignment2/Assignment2Application.java
@@ -3,10 +3,24 @@ package sharma.priyanka.lawendy.abram.wis.hw2.Assignment2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+
 @SpringBootApplication
 public class Assignment2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Assignment2Application.class, args);
-	}
+    public static HashMap<Long, Student> hmStudent;
+
+    public static void main(String[] args) {
+        hmStudent = new HashMap<Long, Student>();
+
+        Student one = new Student("Priyanka Sharma", "WIS");
+        Student two = new Student("Abram Lawendy", "WIS");
+        hmStudent.put(new Long(one.getId()), one);
+        hmStudent.put(new Long(two.getId()), two);
+
+        System.out.print(hmStudent.get(one.getId()));
+        System.out.print(hmStudent.get(two.getId()));
+
+        SpringApplication.run(Assignment2Application.class, args);
+    }
 }
