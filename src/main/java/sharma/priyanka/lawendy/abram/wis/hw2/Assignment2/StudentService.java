a8789dd02ff99931b29200ac040c2040d92ec3e0
Abram Lawendy <abramghandour@gmail.com>
2018-06-09 17:40:10 +0200

    :alien: Sending data from browser to the service

--- src/main/java/sharma/priyanka/lawendy/abram/wis/hw2/Assignment2/StudentService.java
+++ src/main/java/sharma/priyanka/lawendy/abram/wis/hw2/Assignment2/StudentService.java
@@ -2,6 +2,7 @@ package sharma.priyanka.lawendy.abram.wis.hw2.Assignment2;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -13,5 +14,12 @@ class StudentService {
     public HashMap<Long, Student> getAllStudents() {
         return Assignment2Application.hmStudent;
     }
-}
 
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public Student addStudent(@RequestParam(value = "name") String name,
+                              @RequestParam(value = "subject", defaultValue = "unknown") String subject) {
+        Student student = new Student(name, subject);
+        Assignment2Application.hmStudent.put(new Long(student.getId()), student);
+        return student;
+    }
+}
